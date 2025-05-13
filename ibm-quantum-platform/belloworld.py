bbdcee121c9aed1d35cd3ff5e518477cbc80c481
Caleb Luh <calebluh@gmail.com>
2025-05-13 12:29:43 -0500

    Create belloworld.py

---
+++ ibm-quantum-platform/belloworld.py
@@ -0,0 +1,13 @@
+# Qiskit code for first circuit created, named Bello World for its use of a Bell state
+# and a play on words for the overused Hello World joke.
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(2, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[0])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
